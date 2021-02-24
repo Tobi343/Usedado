8cca9892599952666009fbd4d2ff39083fb72cc0
Tobi343 <55580836+Tobi343@users.noreply.github.com>
2021-02-24 15:02:28 +0100

    as

--- NODEJS/node-firebase-functions/functions/index.js
+++ NODEJS/node-firebase-functions/functions/index.js
@@ -1,11 +1,58 @@
 const functions = require("firebase-functions");
 const { Storage } = require("@google-cloud/storage");
+const admin = require("firebase-admin");
 const gcs = new Storage();
 const sharp = require("sharp");
 const fs = require("fs-extra");
 const os = require("os");
 const path = require("path");
 const uuid = require('uuid');
+admin.initializeApp(functions.config().firebase);
+
+/*exports.sendNotification = functions.firestore.document("Activities/{activity_id}").onWrite((change, context) =>{
+
+    console.log("params: "+context.params.OwnerID)
+});
+
+exports.createUser = functions.firestore
+    .document("Activities/{activity_id}")
+    .onCreate((snap, context) => {
+        const newValue = snap.data();
+        const name = newValue.OwnerID;
+        console.log("Name: "+name)
+
+    });*/
+
+exports.updateUser = functions.firestore
+    .document('Activities/{activity_id}')
+    .onUpdate((change, context) => {
+        const newValue = change.after.data();
+        const previousValue = change.before.data();
+        const payload = {
+            notification:{
+                title:"",
+                body: ""
+            }
+        }
+
+       if(previousValue.State == "asked" && newValue.State == "accepted"){
+           console.log("Hey your Offer is accepted!")
+           payload.notification.title = "Hey your Offer is accepted!";
+           payload.notification.body = "Your offer for "+newValue.Title + " is accepted by the Vendor!"
+       }
+        if(previousValue.State == "asked" && newValue.State == "rejected"){
+            console.log("Hey your Offer is rejected!")
+            payload.notification.title = "Hey your Offer is rejected!";
+            payload.notification.body = "Your offer for "+newValue.Title + " is sadly rejected by the Vendor!"
+
+        }
+        console.log("OwnerID: "+newValue.OwnerID);
+        console.log("UserID: "+newValue.UserID);
+        admin.messaging().sendToDevice(newValue.tokenID,payload).then(res=>{
+            console.log("Notification send!")
+        })
+    });
+
 
 exports.resizeImages = functions.storage.object().onFinalize(async object => {
     try {
